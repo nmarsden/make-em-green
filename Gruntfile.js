4b9a8dd222a2364f039716ca40e8f1575f5ab124
nmarsden <nmarsden@gmail.com>
2014-05-19 22:38:10 +1000

    - added levels

--- Gruntfile.js
+++ Gruntfile.js
@@ -119,6 +119,17 @@ module.exports = function ( grunt ) {
           }
        ]   
       },
+      build_vendor_fonts: {
+        files: [
+          {
+            src: [ '<%= vendor_files.fonts %>' ],
+            dest: '<%= build_dir %>/fonts/',
+            cwd: '.',
+            expand: true,
+            flatten: true
+          }
+       ]
+      },
       build_appjs: {
         files: [
           {
@@ -471,7 +482,7 @@ module.exports = function ( grunt ) {
         files: [ 
           'src/assets/**/*'
         ],
-        tasks: [ 'copy:build_app_assets', 'copy:build_vendor_assets' ]
+        tasks: [ 'copy:build_app_assets', 'copy:build_vendor_assets', 'copy:build_vendor_fonts' ]
       },
 
       /**
@@ -553,7 +564,7 @@ module.exports = function ( grunt ) {
    */
   grunt.registerTask( 'build', [
     'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'less:build',
-    'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
+    'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets', 'copy:build_vendor_fonts',
     'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
     'karma:continuous' 
   ]);
--- src/app/menu/menu.js
+++ src/app/menu/menu.js
@@ -17,12 +17,6 @@ angular.module( 'meg.menu', [
 })
 
 .controller( 'MenuCtrl', function MenuCtrl( $scope ) {
-  // This is simple a demo for UI Boostrap.
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
 })
 
 ;
