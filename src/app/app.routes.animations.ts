29f1ff942573be907b5d59c14efa6ae7f7707644
nmarsden <nmarsden@gmail.com>
2017-01-07 16:40:19 +1100

    change WebkitTransform to webkitTransform

--- src/app/app.routes.animations.ts
+++ src/app/app.routes.animations.ts
@@ -8,26 +8,26 @@ function rotateInOut() {
   return trigger('routerTransition', [
     state('void', style({
       opacity: 0,
-      WebkitTransform: 'rotateX(-90deg)',
+      webkitTransform: 'rotateX(-90deg)',
       transform: 'rotateX(-90deg)'
     })),
     state('*', style({
       opacity: 1,
-      WebkitTransform: 'rotateX(0deg)',
+      webkitTransform: 'rotateX(0deg)',
       transform: 'rotateX(0deg)'
     })),
     transition(':enter', [
       // Note: delay the 'enter' animation, so that the previous route's 'leave' animation has time to complete
       animate('0.5s 0.5s ease-in-out', style({
         opacity: 1,
-        WebkitTransform: 'rotateX(0deg)',
+        webkitTransform: 'rotateX(0deg)',
         transform: 'rotateX(0deg)'
       }))
     ]),
     transition(':leave', [
       animate('0.5s ease-in-out', style({
         opacity: 0,
-        WebkitTransform: 'rotateX(-90deg)',
+        webkitTransform: 'rotateX(-90deg)',
         transform: 'rotateX(-90deg)'
       }))
     ])
